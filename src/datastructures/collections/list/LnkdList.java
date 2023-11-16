5bfd2a4b460d47ab7406c5f69ed4a0ffdbb5771f
salaheddinesamid <salaheddine.kobra@iu-study.org>
2023-11-16 20:08:44 +0100

    Remove element from linkedList

--- src/datastructures/collections/list/LnkdList.java
+++ src/datastructures/collections/list/LnkdList.java
@@ -5,5 +5,11 @@ import java.util.List;
 public class LnkdList {
     public static void main(String[] args){
         List<String> lnk = new LinkedList<>();
+        lnk.add("JavaScript");
+        if(lnk.remove("JavaScript")){
+            System.out.println("Element Removed");
+        }
+        lnk.add("C#");
+        System.out.println(lnk.get(0));
     }
 }

92aaadcef3d95bf6b6edd5e84c3b6a5cfecd8ff8
salaheddinesamid <salaheddine.kobra@iu-study.org>
2023-11-18 13:52:10 +0100

    New Function

--- src/datastructures/maps/MAP.java
+++ src/datastructures/maps/MAP.java
@@ -2,24 +2,22 @@ package datastructures.maps;
 
 import javax.xml.crypto.Data;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 public class MAP {
 
     public static void main(String[] args) throws IOException {
         MAP mp = new MAP();
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Type Hello to Continue or Exit to end the program");
-        String userInput = sc.nextLine();
-        while(userInput != null){
+        //Scanner sc = new Scanner(System.in);
+        //System.out.println("Type Hello to Continue or Exit to end the program");
+        //String userInput = sc.nextLine();
+        /*while(userInput != null){
         System.out.println("Please Enter your ID: ");
         String id = sc.nextLine();
         mp.getCiviliansInfo(id);
         userInput = sc.nextLine();
-        }
+        }*/
+        mp.listAllUsers();
 
     }
     public void getCiviliansInfo(String id) throws IOException {
@@ -32,6 +30,14 @@ public class MAP {
             System.out.println("Age:" + d.get(id).age);
         }else{
             System.out.println("Sorry there is no user registered by this ID");
+        }
+    }
+    public void listAllUsers() throws IOException {
+        Database db = new Database();
+        db.generateData();
+        HashMap<String,User> d = db.dataTable;
+        for(Map.Entry<String,User> m : d.entrySet()){
+            System.out.println(m.getKey() + " " + m.getValue().firstName);
         }
     }
 }
